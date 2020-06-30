860382d7ce2711fb9a9531348bed97f17721bd9f
Ryan Locascio <loc.fitness1@gmail.com>
2020-06-30 15:35:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,7 @@ def find_valid_calls(planeteer_calls)
    planeteer_calls.find do |single_planeteer_call|
      valid_calls == single_planeteer_call
   end
+  single_planeteer_call
 end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
